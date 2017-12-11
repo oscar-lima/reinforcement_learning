d49254113356280bedb9b7d8a073a0ecbe88af22
Oscar Lima <olima_84@yahoo.com>
2017-12-11 11:13:11 +0000

    finish action reward function

--- cliff_problem/cliff_problem.py
+++ cliff_problem/cliff_problem.py
@@ -6,8 +6,11 @@ class CliffProblem (object):
     The cliff problem, q-learning and Sarsa
     '''
     def __init__ (self):
+        self.goal_state = 47
+        self.initial_state = 36 # robot starts from cell 36
+        # ---
+        self.robot_state = self.initial_state
         self.world_state = self.create_initial_world_state ()
-        self.robot_state = 36 # robot starts from cell 36
 
 
     def create_initial_world_state (self):
@@ -16,12 +19,10 @@ class CliffProblem (object):
         48 element matrix 4 x 12
         '''
         world_state = []
-        for i in range (0, 36):
+        for i in range (0, 37):
             world_state.append ('E') # E - Empty
 
-        world_state.append ('R') # S - Start
-
-        for i in range (len(world_state), 47):
+        for i in range (len(world_state), self.goal_state):
             world_state.append ('C') # C - cliff
 
         world_state.append ('G') # G - Goal
@@ -35,10 +36,78 @@ class CliffProblem (object):
         '''
         for i, cell in enumerate(self.world_state):
             if i % 12 == 0 and i != 0:
-                print ('\n')
-                print (cell),
+                if robot_state == i:
+                    print '\n'
+                    print 'R',
+                else:
+                    print '\n'
+                    print (cell),
             else:
-                print (cell),
+                if robot_state == i:
+                    print 'R',
+                else:
+                    print (cell),
+
+
+    def robot_is_in_cliff (self, state):
+        '''
+        return true if robot is in the cliff
+        '''
+        if state > 36 and state < self.goal_state:
+            return True
+        else:
+            return False
+        
+
+    def execute_action (self, state, action, world_width=12):
+        '''
+        Execute an action: up, down, left, right.
+        Change the agent state and get a reward
+        '''
+        # ensure state is different than the cliff
+        assert not self.robot_is_in_cliff(state)
+
+        # most states provide with this reward so it makes sense to set it now and only modify if needed
+        reward = -1
+
+        if action == 'up':
+            if state < world_width:
+                print 'agent cant go up because it is at the higher level already, doing nothing'
+            else:
+                state = state - world_width
+        elif action == 'down':
+            if state < self.goal_state - world_width + 1:
+                state = state + world_width
+        elif action == 'left':
+            # handle particular cases
+            if state % world_width == 0:
+                print 'robot is at a extreme left cell, executing left will lead to the same position'
+            else:
+                state = state - 1
+        elif action == 'right':
+            # handle particular cases
+            if state == 11 or state == 23 or state == 35 or state == 47:
+                print 'robot is at extreme right cell, executing right will lead to the same position'
+            else:
+                state = state + 1
+        else:
+            print 'Action is not known, admissible values are: up, down, left, right'
+            return None
+        
+        # if robot is in cliff then send it to intial state (36)
+        if self.robot_is_in_cliff (state):
+            reward = -100
+            state = self.initial_state # 36
+        
+        # ensure state to be between range 0 - goal state
+        assert state < self.goal_state + 1
+        assert state >= 0
+        
+        # if goal is reached after executing action then reward with 100
+        if state == self.goal_state:
+            reward = 100
+        
+        return [state, reward]
 
 
     def compute_reward (self, state):
@@ -47,7 +116,7 @@ class CliffProblem (object):
         every step = -1
         goal       = 100
         '''
-        if state == 47:
+        if state == self.goal_state:
             return 100
         elif state < 37:
             return -1
--- cliff_problem/cliff_unit_test.py
+++ cliff_problem/cliff_unit_test.py
@@ -49,7 +49,218 @@ class TestStringMethods(unittest.TestCase):
         reward = self.cliff_problem_instance.compute_reward (47)
         expected_reward = 100
         self.assertEqual(reward, expected_reward)
+    
+    def test_robot_is_in_cliff3 (self):
+        '''
+        is robot on the cliff?, test the critical points
+        '''
+        expected_value = False
+        output_value = self.cliff_problem_instance.robot_is_in_cliff (36)
+        self.assertEqual(expected_value, output_value)
+        #--
+        expected_value = True
+        output_value = self.cliff_problem_instance.robot_is_in_cliff (37)
+        self.assertEqual(expected_value, output_value)
+        #--
+        expected_value = True
+        output_value = self.cliff_problem_instance.robot_is_in_cliff (41)
+        self.assertEqual(expected_value, output_value)
+        #--
+        expected_value = True
+        output_value = self.cliff_problem_instance.robot_is_in_cliff (46)
+        self.assertEqual(expected_value, output_value)
+        #--
+        expected_value = False
+        output_value = self.cliff_problem_instance.robot_is_in_cliff (47)
+        self.assertEqual(expected_value, output_value)
+        #--
+        expected_value = False
+        output_value = self.cliff_problem_instance.robot_is_in_cliff (0)
+        self.assertEqual(expected_value, output_value)
+    
+    def test_execute_action_left_4 (self):
+        expected_value = 3
+        output_value = self.cliff_problem_instance.execute_action (4, 'left')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 0
+        output_value = self.cliff_problem_instance.execute_action (0, 'left')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 12
+        output_value = self.cliff_problem_instance.execute_action (12, 'left')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 24
+        output_value = self.cliff_problem_instance.execute_action (24, 'left')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 36
+        output_value = self.cliff_problem_instance.execute_action (36, 'left')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 28
+        output_value = self.cliff_problem_instance.execute_action (29, 'left')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 36
+        output_value = self.cliff_problem_instance.execute_action (47, 'left')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-100, output_value[1]) # reward test
+        #--
+        expected_value = 22
+        output_value = self.cliff_problem_instance.execute_action (23, 'left')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 5
+        output_value = self.cliff_problem_instance.execute_action (6, 'left')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+    
+    def test_execute_action_right_4 (self):
+        expected_value = 11
+        output_value = self.cliff_problem_instance.execute_action (11, 'right')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 23
+        output_value = self.cliff_problem_instance.execute_action (23, 'right')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 35
+        output_value = self.cliff_problem_instance.execute_action (35, 'right')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 47
+        output_value = self.cliff_problem_instance.execute_action (47, 'right')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(100, output_value[1]) # reward test
+        #--
+        expected_value = 29
+        output_value = self.cliff_problem_instance.execute_action (28, 'right')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 15
+        output_value = self.cliff_problem_instance.execute_action (14, 'right')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 1
+        output_value = self.cliff_problem_instance.execute_action (0, 'right')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 23
+        output_value = self.cliff_problem_instance.execute_action (22, 'right')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 36
+        output_value = self.cliff_problem_instance.execute_action (36, 'right')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-100, output_value[1]) # reward test
         
+    def test_execute_action_down_5 (self):
+        expected_value = 12
+        output_value = self.cliff_problem_instance.execute_action (0, 'down')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 36
+        output_value = self.cliff_problem_instance.execute_action (25, 'down')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-100, output_value[1]) # reward test
+        #--
+        expected_value = 36
+        output_value = self.cliff_problem_instance.execute_action (28, 'down')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-100, output_value[1]) # reward test
+        #--
+        expected_value = 36
+        output_value = self.cliff_problem_instance.execute_action (31, 'down')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-100, output_value[1]) # reward test
+        #--
+        expected_value = 47
+        output_value = self.cliff_problem_instance.execute_action (35, 'down')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(100, output_value[1]) # reward test
+        #--
+        expected_value = 23
+        output_value = self.cliff_problem_instance.execute_action (11, 'down')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 27
+        output_value = self.cliff_problem_instance.execute_action (15, 'down')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 47
+        output_value = self.cliff_problem_instance.execute_action (47, 'down')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(100, output_value[1]) # reward test
+        #--
+        expected_value = 36
+        output_value = self.cliff_problem_instance.execute_action (36, 'down')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        
+    def test_execute_action_up_6 (self):
+        expected_value = 0
+        output_value = self.cliff_problem_instance.execute_action (0, 'up')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 5
+        output_value = self.cliff_problem_instance.execute_action (5, 'up')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 10
+        output_value = self.cliff_problem_instance.execute_action (10, 'up')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 11
+        output_value = self.cliff_problem_instance.execute_action (11, 'up')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 24
+        output_value = self.cliff_problem_instance.execute_action (36, 'up')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 18
+        output_value = self.cliff_problem_instance.execute_action (30, 'up')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 2
+        output_value = self.cliff_problem_instance.execute_action (14, 'up')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 21
+        output_value = self.cliff_problem_instance.execute_action (33, 'up')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
+        #--
+        expected_value = 35
+        output_value = self.cliff_problem_instance.execute_action (47, 'up')
+        self.assertEqual(expected_value, output_value[0])
+        self.assertEqual(-1, output_value[1]) # reward test
 
 if __name__ == '__main__':
     unittest.main()
